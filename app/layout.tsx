bfb8561c1cc91c8b907c84712339d53a9c0c395d
Teja Chennuru <tejachennuru05@gmail.com>
2025-04-03 20:27:48 +0530

    Update v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,22 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "COGAAN - Coding Gaming and Animation",
   description: "Official website of COGAAN - Coding Gaming and Animation Club",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+  openGraph: {
+    images: [
+      {
+        url: "/images/LogoBlackBg.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "COGAAN Logo",
+      },
+    ],
+  },
+  icons: {
+    icon: "/images/LogoBlackBg.jpeg",
+  },
+};
+
 
 export default function RootLayout({
   children,
